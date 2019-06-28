c31b3bd4498a4a4c90410a969e36fa92081a3bea
André <andre.damasceno@intelltech.com.br>
2019-06-28 09:55:42 -0300

    First commit

--- README.md
+++ README.md
@@ -1,1 +1,48 @@
-# redmineTaskCreator
+﻿# RedmineTaskCreator
+Script que auxilia a criação de tasks no Redmine a partir de uma planilha XLS.
+
+## Planilha
+O formato da planilha deve ser o seguinte ([exemplo](xls/teste.xlsx)):
+
+| Tarefa  | Descrição | Pontos | Requisitos Associados |
+| ------------- | ------------- | ------------- | ------------- |
+| [A] Manter usuários  | Tarefa agregadora  | | RFA0123 |
+| [BACK-END] Cadastrar usuário  | Cadastra um usuário no BD | 8 | RFU0130, RFS0131
+| [FRONT-END] Cadastrar usuário  | Cadastra um usuário na interface | 13 | RFU0130, RFS0131
+| [A] Manter empresa  | Tarefa agregadora  | | RFA0140 |
+| [BACK-END] Cadastrar empresa  | Cadastra uma empresa no BD | 8 | RFU0141
+
+Taregas que iniciam com `[A]` serão marcadas como `WORKPACKAGE` e as tarefas a seguir terão ela marcada como tarefa pai. No exemplo as tarefas teriam a seguinte hierarquia no Redmine:
+
+```
+Manter usuários
+|   [BACK-END] Cadastrar usuário
+|   [FRONT-END] Cadastrar usuário
+Manter empresa
+|   [BACK-END] Cadastrar empresa
+```
+
+A tag `[A]` é removida na hora de cadastrar a tarefa.
+
+## Modo de utilização
+```sh
+python createTasksFromXLSX.py [-h] [-u USER] [-p PARENT]
+                              [-a ASSIGNEDTO] [-v]
+                              filePath
+                              
+positional arguments:
+  filePath              Caminho do arquivo xlsx com as atividades a serem cadastradas
+
+optional arguments:
+  -h, --help            Mostra essa ajuda e sai
+  -u USER, --user USER  Usuário do redmine
+  -p PARENT, --parent PARENT
+                        Id da tarefa pai das tarefas a serem criadas (Id da sprint?)
+  -v, --verbose         Aumenta a verbosidade da saída do programa
+```
+
+A tarefa pai deve estar atribuída para algum usuário pois as tarefas criadas serão atribuídas para este mesmo usuário.
+
+## License
+
+Distributed under the terms of the `MIT` license, "Redmine Task Creator" is free and open source software
---
+++ createTasksFromXLSX.py
@@ -0,0 +1,185 @@
+# -*- coding: UTF-8 -*-
+from __future__ import print_function
+import xlrd, json, requests, getpass, argparse
+
+#----------------------------------------------------------------------
+
+class Issue( object ):
+
+    def __init__( self, project, trackerId, parentId, subject, description, estimated_hours, requisitos, assigned_to ):
+        self.project = project
+        self.tracker_id = trackerId
+        self.parent_issue_id = parentId
+        self.assigned_to = assigned_to
+        self.custom_fields = [ { 'value':'1', 'id':'14' } ] #Situação Planejado
+
+        if requisitos:
+            self.custom_fields.append({ 'id': '3', 'value': requisitos })
+        
+        self.id = None
+        self.status_id= 20  # Status 'To Do'
+        self.priority_id= 2 #Prioridade normal
+
+        self.subject= subject
+        self.description = description
+        self.estimated_hours = estimated_hours
+
+    def toJson(self):
+        dictIssue = {
+                        'project_id': self.project['id'],
+                        'tracker_id': self.tracker_id,
+                        'status_id':  self.status_id,
+                        'priority_id':self.priority_id,
+                        'parent_issue_id': self.parent_issue_id,
+                        'subject': self.subject,
+                        'assigned_to_id': self.assigned_to['id'],
+                        'custom_fields': self.custom_fields
+                    }
+
+        if self.description:
+            dictIssue['description'] = self.description
+
+        if self.estimated_hours:
+            dictIssue['estimated_hours'] = self.estimated_hours
+        
+        return { "issue": dictIssue }
+
+def process_file(path, projectId, parentId, assignedTo):
+    """
+    Open and read an Excel file
+    """
+    book = xlrd.open_workbook(path)
+ 
+    # get the first worksheet
+    first_sheet = book.sheet_by_index(0)
+ 
+    currentWorkPackageId = parentId
+
+    for rx in range( 1, first_sheet.nrows ):
+        row = first_sheet.row(rx)
+        name = row[0].value
+        description = row[1].value
+        points = row[2].value
+        requirements = row[3].value
+
+        if name:
+            workpackage = name.find("[A]") != -1
+            issue = None
+
+            if workpackage:
+                issue = Issue( { 'id' : projectId }, 7, parentId, name[4:], description, points, requirements, { 'id' : assignedTo })
+            else:
+                issue = Issue( { 'id' : projectId }, 16, currentWorkPackageId, name, description, points, requirements, { 'id' : assignedTo })
+
+            issueSaved = postIssues( issue )
+
+            if workpackage:
+                currentWorkPackageId = issueSaved['issue']['id']
+
+
+def postIssues( issue ):
+    payload = json.dumps(issue.toJson())
+    
+    if verbose:
+        print("Cadastrando tarefa: ", issue.subject)
+
+    r = requests.post("http://redmine/projects/time-dev-1/issues.json",
+                    auth=(username, password),
+                    data= payload,
+                    headers = {'content-type': 'application/json'}
+                    )
+
+    checkReturnCode( 201, r.status_code)
+
+    return json.loads(r.text)
+
+def getIssue( issueId ):
+    r = requests.get("http://redmine/projects/time-dev-1/issues/" + str(issueId) + ".json",
+                    auth=(username, password)
+                    )
+
+    checkReturnCode( 200, r.status_code)
+
+    issueDict = json.loads(r.text)
+
+    issue = Issue( issueDict['issue']['project'],
+        issueDict['issue']['tracker']['id'],
+        issueDict['issue']['parent']['id'] if 'parent' in issueDict['issue'] else None,
+        issueDict['issue']['subject'],
+        issueDict['issue']['description'],
+        issueDict['issue']['estimated_hours'] if 'estimated_hours' in issueDict['issue'] else None,
+        issueDict['issue']['custom_fields'][0]['id'] if 'custom_fields' in issueDict['issue'] else None,
+        issueDict['issue']['assigned_to'] if 'assigned_to' in issueDict['issue'] else None
+    )
+
+    return issue
+
+def checkReturnCode( expected, code ):
+    if expected == code:
+        return True
+
+    if code == 401:
+        print( "Erro ao autenticar." )
+        exit()
+    else:
+        print( "Erro: ", code, ". Continuar? [S/n]" )
+    
+    if input() == "n":
+        exit()
+
+    return False
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("filePath", help="Caminho do arquivo xlsx com as atividades a serem cadastradas.")
+    parser.add_argument("-u", "--user", help="Usuário do redmine.")
+    parser.add_argument("-p", "--parent", help="Id da tarefa pai das tarefas a serem criadas (Id da sprint?).", type=int)
+    parser.add_argument("-v", "--verbose", help="Aumenta a verbosidade da saída do programa", action="store_true")
+    args = parser.parse_args()
+
+    global username
+    global password
+    global verbose
+
+    verbose = args.verbose
+
+    if args.user:
+        username = args.user
+        if verbose:
+            print( "Usuário do redmine: ", username )
+    else:
+        username = input("Usuário do redmine: ")
+
+    password = getpass.getpass("Senha: ")
+
+    parentId = args.parent if args.parent else int(input("Id da tarefa pai: "))
+
+    parentIssue = getIssue( parentId )
+
+    if not parentIssue.assigned_to:
+        print( "A tarefa pai deve estar atribuída para alguém para que as tarefas criadas sejam atribuídas para o mesmo usuário. ")
+        exit()
+
+    if verbose:
+        print( "Projeto: " + parentIssue.project['name'] )
+        print( "Tarefa pai: " + parentIssue.subject )
+        print( "Atribuido para: " + parentIssue.assigned_to['name'] )
+
+    process_file( args.filePath, parentIssue.project['id'], parentId, parentIssue.assigned_to['id'])
+    # test()
+
+def test():
+    #parentId = 31221
+    issue = Issue( projectId = 291, trackerId = 16, parentId = 31251, subject = 'Teste completo3' , description = 'Teste',
+        estimated_hours = 3, requisitos = 'RFU0123, RFA0001', assignedTo = 76 )
+    print( issue.toJson() )
+    print( ' --------------------------- ' )
+
+    issueSaved = postIssues( issue )
+    print( issueSaved )
+    id = issueSaved['issue']['id']
+    print( 'id = ', id )
+
+#----------------------------------------------------------------------
+if __name__ == "__main__":
+    main()
